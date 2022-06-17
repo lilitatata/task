8afb9b82180c8d9bc8fe605571e6bd73d3975b95
欧根亲王 <1191986715@qq.com>
2022-06-17 23:32:21 +0800

    '完成了第三步和第四步'

--- src/main.js
+++ src/main.js
@@ -2,3 +2,6 @@ import { marquee } from './marquee.js'
 import { tab } from './tab.js'
 tab()
 marquee()
+
+import './styles/index.css'
+import './styles/index.less'
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { join }= require('path');
 module.exports ={
+    mode:"development",
     entry:"./src/main.js",
     output:{
         path: join(__dirname, 'dist'),
@@ -11,5 +12,19 @@ module.exports ={
         new HtmlWebpackPlugin({
             template:join(__dirname,'public/index.html')
         })
-    ]
+    ],
+    module:{
+        rules:[
+            {
+                test:/\.css/i,
+                // 解析规则:从后向前
+                // style-loader 放在 css-loader之前
+                use:["style-loader","css-loader"]
+            },
+            {
+                test: /\.less$/i,
+                use:["style-loader","css-loader","less-loader"]
+            },
+        ]
+    }
 }
